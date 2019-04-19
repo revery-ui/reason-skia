82a4a5435dab1c8b36bc823c70a18451b3398235
Manuel Hornung <manuel@hornung.in>
2019-04-19 18:15:29 +0200

    Fix build setup

--- src/config/discover.ml
+++ src/config/discover.ml
@@ -1,17 +1,16 @@
 open Configurator.V1
 
-let get_config c =
-  let default = { cflags=[]; libs=[] } in
+let () =
+main ~name:"discover" (fun c ->
+let default: Pkg_config.package_conf = { libs = []; cflags = [] } in
+let config =
   match Pkg_config.get c with
   | None -> default
-  | Some pkg_config -> (
-      match Pkg_config.query pkg_config ~package:"skia" with
-      | None -> default
-      | Some config -> config
+  | Some pkg_config ->
+     match (Pkg_config.query pkg_config ~package:"skia") with
+     | None -> default
+     | Some deps -> deps
 in
 
-let () =
-  main ~name:"discover" (fun c ->
-      let config = get_config c in
-      Flags.write_sexp "cxx_flags.sexp" config.cflags;
-      Flags.write_sexp "c_library_flags.sexp" config.libs )
+Flags.write_sexp "cxx_flags.sexp" config.cflags;
+Flags.write_sexp "c_library_flags.sexp" config.libs)

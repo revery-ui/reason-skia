1626b8ca686df75dbdc443042c4df1682d2d4159
Bryan Phelps <bryphe@outlook.com>
2020-01-15 09:35:05 -0800

    Even more building

--- build/docker-build.sh
+++ build/docker-build.sh
@@ -4,11 +4,16 @@ clang++ -v
 
 esy install
 
+
 esy echo '#{esy-libjpeg-turbo.lib}'
 esy ls '#{esy-libjpeg-turbo.lib}'
 
+echo "-- ESY BUILD"
 esy build
 
+esy ls '#{esy-libjpeg-turbo.lib}'
+
+echo "-- EXAMPLE INSTALL / BUILD"
 esy @example install
 esy @example build
 esy @example run
